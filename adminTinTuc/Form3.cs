c04d8036b04d7c1901488fc7343df030df143ac5
NguyenQuocKhanh <Raycrop555@gmail.com>
2024-08-03 21:22:22 +0700

    Display all information of all accounts in dataViewGrid1

--- adminTinTuc/Form3.cs
+++ adminTinTuc/Form3.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -17,9 +20,10 @@ namespace adminTinTuc
             InitializeComponent();
         }
 
-        private void Form3_Load(object sender, EventArgs e)
+        private async void Form3_Load(object sender, EventArgs e)
         {
-
+            var accounts = await GetAccountsAsync();
+            dataGridView1.DataSource = accounts;
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -36,6 +40,49 @@ namespace adminTinTuc
         {
             var addAccountForm = new AddAccountForm();
             addAccountForm.ShowDialog();
+        }
+
+        private async Task<List<Account>> GetAccountsAsync()
+        {
+            string apiUrl = "https://localhost:7161/api/Account"; // URL của API lấy tất cả tài khoản
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    // Thiết lập token trong header nếu cần
+                    client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", GlobalVariables.JwtToken);
+
+                    HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string responseBody = await response.Content.ReadAsStringAsync();
+                        var accounts = JsonConvert.DeserializeObject<List<Account>>(responseBody);
+                        return accounts;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Failed to retrieve accounts. Status Code: " + response.StatusCode);
+                        return new List<Account>();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                    return new List<Account>();
+                }
+            }
+        }
+
+        public class Account
+        {
+            public string Id { get; set; }
+            public string Email { get; set; }
+            public string Password { get; set; }
+            public string Name { get; set; }
+            public DateTime CreatedAt { get; set; }
+            public DateTime UpdatedAt { get; set; }
+            public string Roles { get; set; }
         }
     }
 }
