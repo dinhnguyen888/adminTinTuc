8bdb4237cb30c1fe0e4229191e352b861b0c187f
NguyenQuocKhanh <Raycrop555@gmail.com>
2024-08-04 20:33:25 +0700

    Add two text boxes to display the description and content of the selected news

--- adminTinTuc/Form4.Designer.cs
+++ adminTinTuc/Form4.Designer.cs
@@ -33,6 +33,8 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.txtContent = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
@@ -55,33 +57,38 @@
             // splitContainer1.Panel2
             // 
             this.splitContainer1.Panel2.Controls.Add(this.dataGridView1);
-            this.splitContainer1.Size = new System.Drawing.Size(1416, 482);
-            this.splitContainer1.SplitterDistance = 172;
+            this.splitContainer1.Panel2.Controls.Add(this.txtDescription);
+            this.splitContainer1.Panel2.Controls.Add(this.txtContent);
+            this.splitContainer1.Size = new System.Drawing.Size(1920, 1055);
+            this.splitContainer1.SplitterDistance = 200;
             this.splitContainer1.TabIndex = 0;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(12, 231);
+            this.button3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button3.Location = new System.Drawing.Point(0, 118);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(152, 59);
+            this.button3.Size = new System.Drawing.Size(200, 59);
             this.button3.TabIndex = 2;
             this.button3.Text = "DELETE NEWS";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(12, 126);
+            this.button2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button2.Location = new System.Drawing.Point(0, 59);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(152, 59);
+            this.button2.Size = new System.Drawing.Size(200, 59);
             this.button2.TabIndex = 1;
             this.button2.Text = "UPDATE NEWS";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 31);
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.Location = new System.Drawing.Point(0, 0);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(152, 59);
+            this.button1.Size = new System.Drawing.Size(200, 59);
             this.button1.TabIndex = 0;
             this.button1.Text = "CREATE NEWS";
             this.button1.UseVisualStyleBackColor = true;
@@ -89,23 +96,46 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(1234, 476);
+            this.dataGridView1.Size = new System.Drawing.Size(1716, 500);
             this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(0, 506);
+            this.txtDescription.Multiline = true;
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtDescription.Size = new System.Drawing.Size(1716, 150);
+            this.txtDescription.TabIndex = 1;
+            this.txtDescription.Text = "Description";
+            // 
+            // txtContent
+            // 
+            this.txtContent.Location = new System.Drawing.Point(-3, 651);
+            this.txtContent.Multiline = true;
+            this.txtContent.Name = "txtContent";
+            this.txtContent.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtContent.Size = new System.Drawing.Size(1716, 404);
+            this.txtContent.TabIndex = 2;
+            this.txtContent.Text = "Content";
             // 
             // Form4
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1416, 482);
+            this.ClientSize = new System.Drawing.Size(1920, 1055);
             this.Controls.Add(this.splitContainer1);
             this.Name = "Form4";
             this.Text = "Form4";
             this.splitContainer1.Panel1.ResumeLayout(false);
             this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
@@ -120,5 +150,7 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.TextBox txtContent;
     }
 }
--- adminTinTuc/Form4.cs
+++ adminTinTuc/Form4.cs
@@ -4,17 +4,71 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json;
 
 namespace adminTinTuc
 {
     public partial class Form4 : Form
     {
+        private static readonly HttpClient client = new HttpClient();
+
         public Form4()
         {
             InitializeComponent();
+            this.WindowState = FormWindowState.Maximized;
+            LoadNewsData();
+        }
+
+        private async void LoadNewsData()
+        {
+            try
+            {
+                var response = await client.GetAsync("https://localhost:7161/api/News");
+                response.EnsureSuccessStatusCode();
+
+                var responseBody = await response.Content.ReadAsStringAsync();
+                var newsList = JsonConvert.DeserializeObject<List<NewsDTO>>(responseBody);
+
+                dataGridView1.DataSource = newsList;
+
+                //dataGridView1.Columns["Title"].DefaultCellStyle.WrapMode = DataGridViewTriState.True;
+                //dataGridView1.Columns["Description"].DefaultCellStyle.WrapMode = DataGridViewTriState.True;
+                //dataGridView1.Columns["Content"].DefaultCellStyle.WrapMode = DataGridViewTriState.True;
+
+                //dataGridView1.Columns["Content"].Width = 700;
+
+                // Auto-size rows to fit the content
+                //dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            }
+            catch (HttpRequestException e)
+            {
+                MessageBox.Show($"Request error: {e.Message}");
+            }
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                var selectedRow = dataGridView1.SelectedRows[0];
+                txtDescription.Text = selectedRow.Cells["Description"].Value.ToString();
+                txtContent.Text = selectedRow.Cells["Content"].Value.ToString();
+            }
+        }
+
+        public class NewsDTO
+        {
+            public string Id { get; set; }
+            public string Title { get; set; }
+            public string LinkDetail { get; set; }
+            public string ImageUrl { get; set; }
+            public string Description { get; set; }
+            public string Content { get; set; }
+            public string Type { get; set; }
         }
     }
 }
